bf64ecf9611197a6dda4a846fecb6a3678bf64cb
Tejaswani97 <tejaswani.chinni7@gmail.com>
2026-09-07 12:30:13 +0530

    Modified CRUD operations in GUI

--- src/util/DatabaseConnection.java
+++ src/util/DatabaseConnection.java
@@ -11,7 +11,7 @@ public class DatabaseConnection {
 
     private static final String USER = "root";
 
-    private static final String PASSWORD = "tejaswani_73";
+    private static final String PASSWORD = "";
 
     public static Connection getConnection() {
 
